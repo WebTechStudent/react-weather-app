948d67a7d50f0ab0f2529f88a7124459e01acfac
WebTechStudent <151663420+WebTechStudent@users.noreply.github.com>
2024-02-17 18:13:52 +0100

    Added 2 files and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App  </div>;
-} 
+  return (
+  <div className="App"> 
+  <div className="container">
+ <Weather />
+  <footer>
+    This project was coded by{" "} 
+    <a href="https://www.shecodes.io/" 
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+  Liudmyla
+  </a>{" "}
+  and is{" "}
+     <a href="https://github.com/WebTechStudent/react-weather-app/tree/main" 
+     target="_blank"
+     rel="noopener noreferrer"
+     > 
+  open-sourced on GitHub
+    </a> 
+  </footer>
+  </div>
+</div>
+ )
+}; 
 
 
